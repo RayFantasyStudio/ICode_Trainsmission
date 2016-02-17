064237078ae63a24ff07a803d7619652fc591e32
ztc1997 <2455484588@qq.com>
2016-02-17 16:30:46 +0800

    MainActivity: 修复setText参数错误

--- app/src/main/java/com/example/qweas/icode_trainsmission/MainActivity.java
+++ app/src/main/java/com/example/qweas/icode_trainsmission/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }*/
                 TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText(result.size());
+                textView.setText(String.valueOf(result.size()));
             }
 
             @Override
